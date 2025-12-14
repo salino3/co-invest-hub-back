c18f344b7b885802f3d949ea5c04ffeae78367a9
Salino3 <flaviobcn012@gmail.com>
2025-12-14 12:22:22 +0200

    extend limit json mb to 50

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // 'morgan' Library to check API call, some information, milliseconds also
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use(
   cors({
