48aa4602f06e4e9d8be449ac1e56d00e0611d380
Salino3 <flaviobcn012@gmail.com>
2026-03-26 19:35:50 +0100

    fix time

--- src/middleware/limiters.js
+++ src/middleware/limiters.js
@@ -1,10 +1,10 @@
 const rateLimit = require("express-rate-limit");
 
-// Set up rate limiter: maximum of 100 requests per 15 minutes per IP
+// Set up rate limiter: maximum of 10000 requests per 15 minutes per IP
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours
+  max: 10000, // Limit each IP to 10000 requests per `window` (here, per 15 minutes)
+  message: "Too many requests from this IP, please try again later",
 });
 
 // Store request counts per IP
